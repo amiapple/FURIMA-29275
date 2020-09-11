b4afd28f5ef21d6d13d20e89a7e3d83c7e31361b
amiapple <amitakabe@gmail.com>
2020-09-10 22:23:38 -0700

    Revert "デプロイ"
    This reverts commit 102eb10ea388dba2a9701ec51be8199b79c529e2.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,3 @@
 Rails.application.routes.draw do
-  get 'items/index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # localhost::3000にアクセスした時にitems#indexが作動するように設定
-  root to: "items#index"
-  # itemsコントローラーを作成し、indexアクションを定義
-  
-  # localhost::3000にアクセスした時にしっかりとトップページが開くか確認
-  # 変更内容をコミット&プッシュ
-  # git push heroku masterコマンドをターミナルで実行
 end
