cfd8a1ae989930e0f8b7fe724e2d9182df751f17
amiapple <amitakabe@gmail.com>
2020-09-14 12:32:35 -0700

    ルーティングとコントローラー編集

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # localhost::3000にアクセスした時にitems#indexが作動するように設定
   root to: "items#index"
+  resources :users, only: [:edit, :update]
   # itemsコントローラーを作成し、indexアクションを定義
   
   # localhost::3000にアクセスした時にしっかりとトップページが開くか確認
