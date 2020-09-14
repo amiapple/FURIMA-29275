51eb43e7a94e07825fee2ce66c3b98b9f4d1a480
amiapple <amitakabe@gmail.com>
2020-09-14 16:36:45 -0700

    バリデーション作業

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,10 @@ class User < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true
   has_many :items
+
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  
+  VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
+  validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
 end
