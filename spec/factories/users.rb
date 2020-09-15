67d605f6e866647f76661c8b0d4f1433d14b4e66
amiapple <amitakabe@gmail.com>
2020-09-15 12:06:42 -0700

    テストコード修正

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -2,7 +2,7 @@ FactoryBot.define do
   factory :user do
     nickname              {Faker::Name.initials}
     email                 {Faker::Internet.free_email}
-    password              {Faker::Internet.password}
-    password_confirmation {password}
+    password              {"password0"}
+    password_confirmation {"password0"}
   end
 end
