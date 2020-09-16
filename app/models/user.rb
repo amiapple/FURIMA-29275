13239c87651cb80d9338814d0935e1752f3488d0
amiapple <amitakabe@gmail.com>
2020-09-15 20:25:06 -0700

    バリデーション追記

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,11 +5,32 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates :nickname, presence: true, uniqueness: true
-  has_many :items
+  has_many :products
+  has_many :comments
+  has_many :product_purchases
 
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
   
   VALID_PASSWORD_REGEX = /\A[a-z0-9]+\z/i
   validates :password, presence: true, length: { minimum: 6 }, format: { with: VALID_PASSWORD_REGEX }
+
+  validates :first_name, format: {
+    with: /\A[^ -~｡-ﾟ]+\z/,
+  },presence: true
+
+  validates :family_name, format: {
+    with: /\A[^ -~｡-ﾟ]+\z/,
+  },presence: true
+
+  validates :first_name_kana, format: {
+    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+  },presence: true
+
+  validates :family_name_kana,format: {
+    with: /\A[\p{katakana} ー－&&[^ -~｡-ﾟ]]+\z/,
+  },presence: true
+
+  validates :birth_day, presence: true
+
 end
