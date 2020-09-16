8ddb526d51993d0cc5282ecd0570b73c36ded7a8
amiapple <amitakabe@gmail.com>
2020-09-15 20:59:32 -0700

    FactoryBot追記

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -4,5 +4,11 @@ FactoryBot.define do
     email                 {Faker::Internet.free_email}
     password              {"password0"}
     password_confirmation {"password0"}
+    first_name            {"太郎"}
+    family_name           {"山田"}
+    first_name_kana       {"タロウ"}
+    family_name_kana      {"ヤマダ"}
+    birthday              {"1930-01-01"}
+
   end
 end
