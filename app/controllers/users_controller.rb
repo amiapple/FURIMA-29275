58c5282c5999ac90cd7f657e4cf273315cb07f59
amiapple <amitakabe@gmail.com>
2020-09-15 18:52:50 -0700

    画面遷移記述

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -14,6 +14,6 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:name, :email)
+    params.require(:user).permit(:nickname, :email, :encrypted_password, :first_name, :family_name, :first_name_kana, :family_name_kana, :birthday)
   end
 end
