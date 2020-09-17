29487f09ba455769ede3fa054f5ce2f648838baa
amiapple <amitakabe@gmail.com>
2020-09-16 17:12:15 -0700

    コントローラー記述

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,18 @@
 class ItemsController < ApplicationController
   skip_before_action :authenticate_user!, only: :index 
   def index
-    
+
   end
 
   def edit
+  end
+
+  def update
+    if current_user.update(user_params)
+      redirect_to root_path
+    else
+      render :edit
+    end
   end
 
 end
