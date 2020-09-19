1f536485e642284cb4292b080ac37dbf99328e70
amiapple <amitakabe@gmail.com>
2020-09-18 19:19:51 -0700

    販売価格実装2

--- app/javascript/input.js
+++ app/javascript/input.js
@@ -1,21 +1,23 @@
 function input() {
-  const posts = document.querySelectorAll(".post");
-  posts.forEach(function (post) {
-// ①フォームに文字が入ったらイベントが発火する
-    post.addEventListener("input", () => { 
-    console.log("integer")
-// ②入力した値（数字）を取得する
-const integer = new integer(document.getElementById("form"));
+  
+  
 
+  
+// ①フォームに文字が入ったらイベントが発火する
+value_input=document.getElementById("item-price")
+    value_input.addEventListener("input", () => {    
+// ②入力した値（数字）を取得する
+value_input = new integer(document.getElementById("item-price"));
+console.log("integer")
 // ③取得した数字を使って、利益と手数料を計算する
-commission = integer*0.1
+commission = integer * 0.1
 profit = integer - commission
 // ④計算結果を表示する
-const commission
-const profit
+commission=new (document.getElementById("item-price"));
+profit=new integer(document.getElementById("item-price"));
 
     });
-  });
+
 }
 window.addEventListener("load", input);
 
