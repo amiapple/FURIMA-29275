5f7e6de2e1e5ebeb092c843ed1675bc44cc27b3a
amiapple <amitakabe@gmail.com>
2020-09-18 17:59:21 -0700

    カテゴリー実装

--- db/migrate/20200916192249_create_products.rb
+++ db/migrate/20200916192249_create_products.rb
@@ -15,3 +15,14 @@ class CreateProducts < ActiveRecord::Migration[6.0]
     end
   end
 end
+
+class CreateArticles < ActiveRecord::Migration[6.0]
+  def change
+    create_table :category do |t|
+      t.string     :title        , null: false
+      t.text       :text         , null: false
+      t.integer    :genre_id     , null: false
+      t.timestamps
+    end
+  end
+ end
