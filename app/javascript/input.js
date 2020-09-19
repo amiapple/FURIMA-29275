38c5445b37ec5fd83b09f05f77525d9ae6048f6a
amiapple <amitakabe@gmail.com>
2020-09-18 21:56:01 -0700

    販売価格実装3

--- app/javascript/input.js
+++ app/javascript/input.js
@@ -1,20 +1,23 @@
 function input() {
-  
-  
-
-  
+ 
 // ①フォームに文字が入ったらイベントが発火する
 value_input=document.getElementById("item-price")
     value_input.addEventListener("input", () => {    
 // ②入力した値（数字）を取得する
-value_input = new integer(document.getElementById("item-price"));
-console.log("integer")
+const courseLists = document.getElementById("item-price").value;
+
 // ③取得した数字を使って、利益と手数料を計算する
-commission = integer * 0.1
-profit = integer - commission
+commission = courseLists * 0.1
+
+profit = courseLists - commission
+
 // ④計算結果を表示する
-commission=new (document.getElementById("item-price"));
-profit=new integer(document.getElementById("item-price"));
+const p1 = document.getElementById("add-tax-price");
+p1.textContent=commission
+
+const p2 = document.getElementById("profit");
+p2.textContent=profit
+
 
     });
 
