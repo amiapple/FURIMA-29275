fc4826681292ff791b00b94145cb28d4bdcdd333
amiapple <amitakabe@gmail.com>
2020-09-24 23:08:34 -0700

    バリデーション記述

--- app/models/product_purchase.rb
+++ app/models/product_purchase.rb
@@ -2,4 +2,15 @@ class ProductPurchase < ApplicationRecord
   belongs_to :user
   belongs_to :products
   has_one :product_purchase
+
+  validates :number, presence: true
+  validates :exp_month, presence: true
+  validates :exp_year, presence: true
+  validates :cvc, presence: true
+  validates :postal_code, presence: true
+  validates :prefecture, presence: true
+  validates :city, presence: true
+  validates :addresses, presence: true
+  validates :building, presence: true
+  validates :phone_number, presence: true
 end
