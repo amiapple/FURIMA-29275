0a2ae9d9f04fa98dbc5a98c2e29d4e602ac1f524
amiapple <amitakabe@gmail.com>
2020-09-24 18:00:36 -0700

    ビュー記述

--- app/controllers/product_purchases_controller.rb
+++ app/controllers/product_purchases_controller.rb
@@ -2,10 +2,11 @@ class ProductPurchasesController < ApplicationController
   before_action :authenticate_user!
   def index
     @product_purchases = Product.find(params[:product_id])
+    @products = Product.find(params[:product_id])
   end
 
   private
-  def product_params
+  def product_purchases_params
     params.require(:product_purchases).permit(:user_id, :product_id).merge(user_id: current_user.id)
   end
 end
