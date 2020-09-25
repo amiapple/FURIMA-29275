4c64a3ba52d7c46a95839f4609388779c33e8854
amiapple <amitakabe@gmail.com>
2020-09-25 15:38:10 -0700

    formwith実装2

--- app/forms/product_purchases_sending_destinations.rb
+++ app/forms/product_purchases_sending_destinations.rb
@@ -3,19 +3,18 @@ class ProductPurchasesSendingDestinations
   include ActiveModel::Model
   attr_accessor :user_id, :product_id, :product_purchases, :postal_code, :shipping_area_id, :city, :addresses, :building, :phone_number
 
-  #product_purchasesのバリデーション
+  #sending_destinationsのバリデーション 
   with_options presence: true do
     validates :number, numericality: { with:/\A[0-9]+\z/}, length: { is: 16 }
     validates :exp_month, numericality: { with:/\A[0-9]+\z/, greater_than_or_equal_to: 1, less_than_or_equal_to: 2}
     validates :exp_year, numericality: { with:/\A[0-9]+\z/}, length: { is: 2 }
     validates :cvc, numericality: { with:/\A[0-9]+\z/}, length: { is: 3 }
     validates :postal_code,numericality: { /\A\d{3}[-]\d{4}\z/}
-    validates :prefecture
+    validates :shipping_area_id
     validates :city
     validates :addresses
     validates :phone_number, numericality: { with:/\A[0-9]+\z/, less_than_or_equal_to: 11}
     end
 
-   #sending_destinationsのバリデーション 
-   
+
 end
