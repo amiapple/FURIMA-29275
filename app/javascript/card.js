2f49a2985ec99903c843f6394830000dfebead9c
amiapple <amitakabe@gmail.com>
2020-09-24 19:46:19 -0700

    javascript記述2

--- app/javascript/card.js
+++ app/javascript/card.js
@@ -1,13 +1,24 @@
 const pay = ()=> {
 Payjp.setPublicKey("pk_test_95dac0017fa40cce27907d22");　// PAY.JPテスト公開鍵
-const form = document.getElementById("item-explain-box");
-form.addEventListener("submit", () => {
-  console.log("イベント発火");
+const form = document.getElementById("charge-form");
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  const formResult = document.getElementById("charge-form");
+  const formData = new FormData(formResult);
+
+  const card = {
+    number: formData.get("number"),
+    cvc: formData.get("cvc"),
+    exp_month: formData.get("exp_month"),
+    exp_year: `20${formData.get("exp_year")}`,
+  };
+
+  Payjp.createToken(card, (status, response) => {
+    console.log(status)
+    console.log(response)
+  })
 })
 }
 
 window.addEventListener("load", pay);
-
-
-
-
