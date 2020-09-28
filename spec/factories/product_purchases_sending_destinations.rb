f65f6061df098f7d497f2ae7fd58fdca4be232b3
amiapple <amitakabe@gmail.com>
2020-09-27 18:14:39 -0700

    テストコード記述3

--- spec/factories/product_purchases_sending_destinations.rb
+++ spec/factories/product_purchases_sending_destinations.rb
@@ -1,9 +1,10 @@
 FactoryBot.define do
-  factory :sending_destination do
+  factory :product_purchases_sending_destination do
     postal_code              {'123-4567' }
     shipping_area_id         {'2'}
     city                     {'札幌市'}
     addresses                {'中央区'}
     phone_number             {'09012345678'}
+    token                    {'sample123456'}
   end
 end
